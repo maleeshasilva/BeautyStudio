a243e1f86eec14b922d41fe5dc059cd54d5e8eb3
maleeshasilva <maleeshadinolisilva@gmail.com>
2021-09-28 09:57:44 +0530

    login.js

---
+++ screens/login.js
@@ -0,0 +1,51 @@
+import React, { useState, useContext } from 'react';
+import { StatusBar } from 'expo-status-bar';
+
+// formik
+import { Formik } from 'formik';
+
+import {
+  StyledContainer,
+  PageLogo,
+  PageTitle,
+  SubTitle,
+  StyledInputLabel,
+  StyledFormArea,
+  StyledButton,
+  StyledTextInput,
+  LeftIcon,
+  RightIcon,
+  InnerContainer,
+  ButtonText,
+  MsgBox,
+  Line,
+  ExtraView,
+  ExtraText,
+  TextLink,
+  TextLinkContent,
+  Colors,
+} from './../components/styles';
+import { View, ActivityIndicator } from 'react-native';
+
+//colors
+const { darkLight, brand, primary } = Colors;
+
+// icon
+import { Octicons, Fontisto, Ionicons } from '@expo/vector-icons';
+
+// keyboard avoiding view
+import KeyboardAvoidingWrapper from './../components/KeyboardAvoidingWrapper';
+
+
+
+const   Login=()=>{
+    return(
+        <StyledContainer>
+            <InnerContainer>
+            <PageLogo resizeMode="cover" source={require('./../assets/img/expo-bg1.png')} />
+            <PageTitle>Beauty Studio</PageTitle>
+            <SubTitle>Account Login</SubTitle>
+            </InnerContainer>
+        </StyledContainer>
+    )
+}
