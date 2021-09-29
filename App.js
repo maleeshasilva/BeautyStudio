a0b34180bfab4e6cd22e365a82194bba8d9ebce5
maleeshasilva <maleeshadinolisilva@gmail.com>
2021-09-29 11:42:11 +0530

    fix the error in RNCSafeAreaProvider

--- App.js
+++ App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 
-
-//screens
-import Login from './screens/Login';
-import Signup from './screens/Signup';
-import Welcome from './screens/Welcome';
+//react navigation stack
+import RootStack from './navigators/RootStack';
 
 export default function App() {
   return (
-    <Welcome/>
+    <RootStack/>
   );
 }
 
--- navigators/RootStack.js
+++ navigators/RootStack.js
@@ -1,1 +1,30 @@
- 
+import React from 'react';
+
+//colors
+import { Colors } from './../components/styles';
+const { darkLight, brand, primary, tertiary, secondary } = Colors;
+
+// React Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+//screens
+import Login from './../screens/Login';
+import Signup from './../screens/Signup';
+import Welcome from './../screens/Welcome';
+
+const Stack = createStackNavigator();
+
+const RootStack =()=>{
+    return(
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Login" component={Login}/>
+                <Stack.Screen name="Signup" component={Signup}/>
+                <Stack.Screen name="Welcome" component={Welcome}/>
+
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+export default RootStack;
